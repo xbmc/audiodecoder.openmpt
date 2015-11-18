8b680eb4de483c9bb6bce3391ace8956e8d74f5a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2015-11-18 15:59:20 +0100

    update include paths

--- src/OpenMptCodec.cpp
+++ src/OpenMptCodec.cpp
@@ -18,12 +18,12 @@
  *
  */
 
-#include "kodi/libXBMC_addon.h"
+#include "libXBMC_addon.h"
 
 extern "C" {
 #include <libopenmpt/libopenmpt.h>
-#include "kodi/kodi_audiodec_dll.h"
-#include "kodi/AEChannelData.h"
+#include "kodi_audiodec_dll.h"
+#include "AEChannelData.h"
 
 ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
 
