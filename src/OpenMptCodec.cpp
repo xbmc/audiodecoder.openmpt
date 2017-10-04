da9ef9373ee38e9ef40c8965c3f6c4462adb81ba
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-10-04 11:37:46 +0200

    update to c++ bindings

--- src/OpenMptCodec.cpp
+++ src/OpenMptCodec.cpp
@@ -18,172 +18,105 @@
  *
  */
 
-#include "libXBMC_addon.h"
+#include <kodi/addon-instance/AudioDecoder.h>
+#include <kodi/Filesystem.h>
 
-extern "C" {
 #include <libopenmpt/libopenmpt.h>
-#include "kodi_audiodec_dll.h"
-#include "AEChannelData.h"
 
-ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
-
-//! \brief Create addon
-//! \details Called on load. Addon should fully initalize or return error status
-ADDON_STATUS ADDON_Create(void* hdl, void* props)
+static size_t vfs_file_fread( void * handle, void* dst, size_t size)
 {
-  if (!XBMC)
-    XBMC = new ADDON::CHelper_libXBMC_addon;
-
-  if (!XBMC->RegisterMe(hdl))
-  {
-    delete XBMC, XBMC=NULL;
-    return ADDON_STATUS_PERMANENT_FAILURE;
-  }
-
-  return ADDON_STATUS_OK;
+  kodi::vfs::CFile* file = static_cast<kodi::vfs::CFile*>(handle);
+  return file->Read(dst, size);
 }
 
-//! \brief Destroy addon
-//! \details Do everything before unload of this add-on
-void ADDON_Destroy()
+static int vfs_file_fseek( void * handle, int64_t offset, int whence )
 {
-  XBMC=NULL;
+  kodi::vfs::CFile* file = static_cast<kodi::vfs::CFile*>(handle);
+  return file->Seek(offset, whence) > -1 ? 0 : -1;
 }
 
-//! \brief Get status of addon
-//! \details Returns the current Status of this audio decoder
-ADDON_STATUS ADDON_GetStatus()
+static int64_t vfs_file_ftell( void * handle )
 {
-  return ADDON_STATUS_OK;
-}
-
-//! \brief Set the value of a given setting
-//! \details Set a specific Setting value (called from XBMC)
-ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void* value)
-{
-  return ADDON_STATUS_OK;
-}
-
-static size_t psf_file_fread( void * handle, void* dst, size_t size)
-{
-  return XBMC->ReadFile(handle, dst, size);
-}
-
-static int psf_file_fseek( void * handle, int64_t offset, int whence )
-{
-  return XBMC->SeekFile(handle, offset, whence) > -1?0:-1;
-}
-
-static int64_t psf_file_ftell( void * handle )
-{
-  return XBMC->GetFilePosition(handle);
-}
-
-#define SET_IF(ptr, value) \
-{ \
-  if ((ptr)) \
-   *(ptr) = (value); \
+  kodi::vfs::CFile* file = static_cast<kodi::vfs::CFile*>(handle);
+  return file->GetPosition();
 }
 
 struct MPTContext
 {
-  openmpt_module* module;
-  void* file;
+  openmpt_module* module = nullptr;
+  kodi::vfs::CFile file;
 };
 
-//! \brief Initialize an audio decoder for a given file
-void* Init(const char* strFile, unsigned int filecache, int* channels,
-           int* samplerate, int* bitspersample, int64_t* totaltime,
-           int* bitrate, AEDataFormat* format, const AEChannel** channelinfo)
+class CMPTCodec : public kodi::addon::CInstanceAudioDecoder,
+                  public kodi::addon::CAddonBase
 {
-  if (!strFile)
-    return NULL;
+public:
+  CMPTCodec(KODI_HANDLE instance) :
+    CInstanceAudioDecoder(instance) {}
 
-  MPTContext* result = new MPTContext;
-  if (!result)
-    return NULL;
-
-  result->file = XBMC->OpenFile(strFile,0);
-  if (!result->file)
+  virtual ~CMPTCodec()
   {
-    delete result;
-    return NULL;
+    if (ctx.module)
+      openmpt_module_destroy(ctx.module);
   }
 
-  static openmpt_stream_callbacks callbacks = { psf_file_fread, psf_file_fseek, psf_file_ftell };
-
-  result->module = openmpt_module_create(callbacks, result->file, openmpt_log_func_silent, NULL, NULL);
-  if (!result->module)
+  virtual bool Init(const std::string& filename, unsigned int filecache,
+                    int& channels, int& samplerate,
+                    int& bitspersample, int64_t& totaltime,
+                    int& bitrate, AEDataFormat& format,
+                    std::vector<AEChannel>& channellist) override
   {
-    XBMC->CloseFile(result->file);
-    delete result;
-    return NULL;
-  }
+    if (!ctx.file.OpenFile(filename,READ_CACHED))
+      return false;
 
-  SET_IF(channels, 2)
-  SET_IF(samplerate, 48000)
-  SET_IF(bitspersample, 32)
-  SET_IF(totaltime,(int64_t)(openmpt_module_get_duration_seconds(result->module)*1000));
-  SET_IF(format, AE_FMT_FLOAT)
-  static enum AEChannel map[3] = {
-    AE_CH_FL, AE_CH_FR, AE_CH_NULL
-  };
-  SET_IF(channelinfo, map)
-  SET_IF(bitrate, openmpt_module_get_num_channels(result->module));
+    static openmpt_stream_callbacks callbacks = { vfs_file_fread, vfs_file_fseek, vfs_file_ftell };
 
-  return result;
-}
+    ctx.module = openmpt_module_create2(callbacks, &ctx.file, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+    if (!ctx.module)
+      return false;
 
-//! \brief Return decoded data
-int ReadPCM(void* context, uint8_t* pBuffer, int size, int *actualsize)
-{
-  if (!context)
-    return 1;
+    channels = 2;
+    samplerate = 48000;
+    bitspersample = 32;
+    totaltime = openmpt_module_get_duration_seconds(ctx.module)*1000;
+    format = AE_FMT_FLOAT;
+    channellist = { AE_CH_FL, AE_CH_FR };
+    bitrate = openmpt_module_get_num_channels(ctx.module);
 
-  MPTContext* ctx = (MPTContext*)context;
-
-  if ((*actualsize = openmpt_module_read_interleaved_float_stereo(ctx->module, 48000, size/8, (float*)pBuffer)*8) == size)
-    return 0;
-  
-  return 1;
-}
-
-//! \brief Seek to a given time
-int64_t Seek(void* context, int64_t time)
-{
-  if (!context)
-    return -1;
-
-  MPTContext* ctx = (MPTContext*)context;
-
-  return openmpt_module_set_position_seconds(ctx->module, time/1000.0)*1000.0;
-}
-
-//! \brief Deinitialize decoder
-bool DeInit(void* context)
-{
-  if (!context)
     return true;
+  }
 
-  MPTContext* ctx = (MPTContext*)context;
-  openmpt_module_destroy(ctx->module);
-  XBMC->CloseFile(ctx->file);
+  virtual int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  {
+    if ((actualsize = openmpt_module_read_interleaved_float_stereo(ctx.module, 48000, size/8, (float*)buffer)*8) == size)
+      return 0;
 
-  delete ctx;
+    return 1;
+  }
 
-  return true;
-}
+  virtual int64_t Seek(int64_t time) override
+  {
+    return openmpt_module_set_position_seconds(ctx.module, time/1000.0)*1000.0;
+  }
 
-//! \brief Returns any tag values for file
-bool ReadTag(const char* strFile, char* title, char* artist,
-             int* length)
+private:
+  MPTContext ctx;
+};
+
+
+class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
-  return false;
-}
+public:
+  CMyAddon() { }
+  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  {
+    addonInstance = new CMPTCodec(instance);
+    return ADDON_STATUS_OK;
+  }
+  virtual ~CMyAddon()
+  {
+  }
+};
 
-//! \brief Returns track count for a given file
-int TrackCount(const char* strFile)
-{
-  return 1;
-}
-}
+
+ADDONCREATOR(CMyAddon);
